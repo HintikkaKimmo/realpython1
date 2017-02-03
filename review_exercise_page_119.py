914c61c124727fccdec940de8fd8bebcc5891b8c
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-03 18:35:49 +0800

    completed review excercise page 119

---
+++ review_exercise_page_119.py
@@ -0,0 +1,11 @@
+print('AAA'.find('a'))
+
+string = 'version'
+floater = 2.0
+
+version = string + ' ' + str(floater)
+
+print(version, 'and location of 2.0 in string array', version.find('2.0'))
+
+
+
