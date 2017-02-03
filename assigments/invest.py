5887ffd973fbfdbb15da5c69a6800d991251a8d3
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-03 19:50:24 +0800

    added print formating

--- assigments/invest.py
+++ assigments/invest.py
@@ -4,9 +4,11 @@
 def invest(amount, rate, time):
     print('Principal amount ${}'.format(amount))
     print('annual rate of return: {}'.format(rate))
+    print()
     for t in range(0, time + 1):
         amount = amount + (amount * rate)
         print('Year {}: ${}'.format(t, amount))
+        print()
 
 invest(100, 0.05, 8)
 invest(2000, 0.25, 5)
