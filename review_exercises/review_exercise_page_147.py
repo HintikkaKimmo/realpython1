9c23e8854dc973d102c49c4a2275df2d33fd4f7b
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-03 19:16:39 +0800

    completed review exercise page 147

---
+++ review_exercises/review_exercise_page_147.py
@@ -0,0 +1,13 @@
+def cube(num):
+    result = num * num * num
+    return result
+
+print(cube(4))
+
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+multiply1 = multiply(2, 5)
+print(multiply1)
