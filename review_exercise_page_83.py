34e66bcd16d50654597ffe160d527c49628e4e13
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-03 17:54:29 +0800

    exersice page 83 done

---
+++ review_exercise_page_83.py
@@ -0,0 +1,13 @@
+# Review exercise page 83 Real Python part 1
+
+review = 'long_string'
+print(len(review))
+
+review2 = 'longer_string'
+print(review + ' ' + review2)
+
+print(review, review2)
+
+zing = 'bazinga'
+print(zing[2:-1])
+
