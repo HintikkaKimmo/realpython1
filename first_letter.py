c69e681e4dce23baf1902591abf4a2be20f42d0e
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-03 18:07:30 +0800

    added handling of empty string on first_letter.py

--- first_letter.py
+++ first_letter.py
@@ -1,4 +1,7 @@
 # Assignment page 95 Real Python part 1
 
 user_input = str(input('Tell me your password: '))
-print('Your passwords first letter in UPPERCASE is', user_input[0].upper())
+if user_input != '':
+    print('Your passwords first letter in UPPERCASE is', user_input[0].upper())
+else:
+    print('You need to answer something!')
