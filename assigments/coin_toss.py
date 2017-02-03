6a427e1a2b59085e86c371b0290c6d9ff21f8e18
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-03 23:27:56 +0800

    tried to debug the coin_flip continue tomorrow

--- assigments/coin_toss.py
+++ assigments/coin_toss.py
@@ -1,14 +1,19 @@
 from random import randint
+# import pdb
 
 flips = 0
 
+# pdb.set_trace()
+
 for tries in range(0, 10000):
+
     flip = randint(0, 1)
     flips += 1
     while flip != randint(0, 1):
+        print(flip)
         flips += 1
 
 print(flips)
 print('On average you need {} to get 2 of the same'.format((flips / 10000)))
 
-# TODO this is giving avarage of 2 needed flips which is wrong need to check the logic
+# TODO this is giving average of 2 needed flips which is wrong need to check the logic
